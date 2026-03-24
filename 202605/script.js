d5f78b77c0db95476031f1e727eec3224daff741
kefir <58820499+alpha-gun@users.noreply.github.com>
2026-03-24 17:42:35 +0900

    Delete 202605 directory

--- 202605/script.js
+++
@@ -1,8 +0,0 @@
-const videoElement = $('.sample_video').get(0);
- 
-$('.sample_play_btn').on('click', function(){
-	videoElement.play();
-});
-$('.sample_pause_btn').on('click', function(){
-	videoElement.pause();
-});
